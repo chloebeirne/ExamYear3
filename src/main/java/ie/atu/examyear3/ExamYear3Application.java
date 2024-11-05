d3dc6eb8d41ea5191684297f664d6fd21f15fe22
G00399850@atu.ie <G00399850@atu.ie>
2024-11-05 10:09:15 +0000

    Creating a new project for exam

---
+++ src/main/java/ie/atu/examyear3/ExamYear3Application.java
@@ -0,0 +1,13 @@
+package ie.atu.examyear3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamYear3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamYear3Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/examyear3/ExamYear3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.examyear3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamYear3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
